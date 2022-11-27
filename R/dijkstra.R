ce06656fbe9ffcde7abb4653ecb7e91b777adab1
Samuel ORTION <samuel.ortion@orange.fr>
2022-11-27 08:05:23 +0100

    Added Dijkstra in the game

--- R/dijkstra.R
+++ R/dijkstra.R
@@ -1,3 +1,7 @@
+# Shortest path in a Graph - AlgoR Project
+# Samuel ORTION
+# GPLv3+
+
 
 #' Bellman-Ford algorithm (R version)
 #'
@@ -5,11 +9,12 @@
 #' @param summits a vector of all summits
 #' @param edges a nested list, which items are composed of `from`, `to` and `weight`
 #' @param start the node which distance from is computed
+#' @param end the target node
 #' @return a list with `d` the constructed distance for each node and `pred` a vector containing at item i the predecessor of i-th summit
-bellmanford <- function(summits, edges, start) {
+bellmanford <- function(summits, edges, start, end) {
   nodedistance <- rep(Inf, length(summits))
-  predecessor <- rep(NULL, length(summits))
   nodedistance[start] <- 0
+  predecessor <- rep(NULL, length(summits))
   predecessor[start] <- start
   for (k in 1:length(summits)) {
     for (edge in edges) {
@@ -36,6 +41,49 @@ bellmanford <- function(summits, edges, start) {
   }
 
   return(list(d = nodedistance, pred = predecessor))
+}
+
+#' Dijkstra Algorithm
+#' @param summits the vector of vertices
+#' @param edges a list of 3-uplet list (from, to, weight) representing the weighted graph
+#' @param start the start node
+#' @param end the target node
+#' @return a list with `d` the constructed distance for each node and `pred` a vector containing at item i the predecessor of i-th summit
+dijkstra <- function(summits, edges, start, end) {
+  visited_nodes <- NULL
+  nodedistance <- rep(Inf, length(summits))
+  nodedistance[start] <- 0
+  predecessor <- rep(NULL, length(summits))
+  predecessor[start] <- start
+
+  while (length(visited_nodes) < length(summits)) {
+    current <- nextclosestnode(summits, visited_nodes, nodedistance)
+    visited_nodes <- c(visited_nodes, current)
+    if (current == end) {
+      break
+    }
+    for (edge in edges) {
+      if (edge$from == current) {
+        dist <- nodedistance[current] + edge$weight
+        if (dist < nodedistance[edge$to]) {
+          nodedistance[edge$to] <- dist
+          predecessor[edge$to] <- edge$from
+        }
+      }
+    }
+  }
+  return(list(
+    d = nodedistance[end], pred = predecessor
+  ))
+}
+
+nextclosestnode <- function(summits, visited, distance) {
+  nodeleft <- which(!(summits %in% visited))
+  distleft <- distance[nodeleft]
+  mindist <- min(distleft)
+  return(
+    which(distance == mindist)[1]
+  )
 }
 
 
--- tests/testthat/test-dijkstra.R
+++ tests/testthat/test-dijkstra.R
@@ -1,4 +1,4 @@
-test_that("Bellman-Ford gives the proper shortest path", {
+test_that("Dijkstra gives the proper shortest path", {
 
   summits <- c(1, 2, 3)
   edges <- list(
@@ -7,7 +7,7 @@ test_that("Bellman-Ford gives the proper shortest path", {
     list(from = 3, to = 2, weight = 2)
   )
 
-  result <- bellmanford(summits, edges, 1, 2)
+  result <- dijkstra(summits, edges, 1, 2)
 
   path <- shortestpath(result$pred, 1, 2)
 
